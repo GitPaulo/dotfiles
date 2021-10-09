d199b5752bbc2913ecf3f1d9a814ab907ff25be6
Paulo <GitPaulo@users.noreply.github.com>
2021-10-09 16:39:44 +0100

    Add stow to prerequisites

--- installations/_prerequisites.sh
+++ installations/_prerequisites.sh
@@ -6,5 +6,8 @@ sudo apt upgrade -y
 
 echo "Installing prerequisites..."
 
+# Stow 
+sudo apt install stow
+
 # fish
 sudo apt install build-essential cmake ncurses-dev libncurses5-dev libpcre2-dev gettext
