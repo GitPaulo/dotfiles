c0f0bc9d7c43d5f4ae9bbb0a5a79ef645dc50de7
Paulo <GitPaulo@users.noreply.github.com>
2021-10-10 15:16:19 +0100

    Update nerd_fonts.sh

--- terminals/fonts/nerd_fonts.sh
+++ terminals/fonts/nerd_fonts.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-fname=$1
-
-if [ $# -eq 0 ]; then
-  read -e -p "Font name?" fname
-fi
-
-curl -fLo "${fname} Nerd Font Complete.otf" \
-    https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/<FONT_PATH>/complete/<FONT_NAME>%20Nerd%20Font%20Complete.otf
+echo "curl -fLo "<FONT NAME> Nerd Font Complete.otf" \
+    https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/<FONT_PATH>/complete/<FONT_NAME>%20Nerd%20Font%20Complete.otf"
