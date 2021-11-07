86bafd22c882bd2df56de0d81b9eaa539e0abb4c
Paulo <GitPaulo@users.noreply.github.com>
2021-11-07 21:08:15 +0000

    jetbrains mono regular

--- terminals/fonts/nerd_fonts.sh
+++ terminals/fonts/nerd_fonts.sh
@@ -2,3 +2,6 @@
 
 echo "curl -fLo "<FONT NAME> Nerd Font Complete.otf" \
     https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/<FONT_PATH>/complete/<FONT_NAME>%20Nerd%20Font%20Complete.otf"
+
+# TODO: Make this easier
+echo "https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/JetBrainsMono/Ligatures/Regular/complete/JetBrains%20Mono%20Regular%20Nerd%20Font%20Complete%20Mono%20Windows%20Compatible.ttf"
