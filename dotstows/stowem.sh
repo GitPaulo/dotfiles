b2d35cf9f7ad87679e441a12025532a660482ad1
Paulo <GitPaulo@users.noreply.github.com>
2021-11-08 05:39:02 +0000

    hmm

--- dotstows/stowem.sh
+++ dotstows/stowem.sh
@@ -9,7 +9,7 @@ then
 fi
 
 echo 'Removing default dotfiles...'
-rm ~/.bash_profile ~/.bashrc ~/.curlrc ~/.gitconfig ~/.vimrc ~/.wgetrc ~/.config/neofetch/config.conf ~/.config/fish/config.fish ~/.config/ranger/rc.conf
+rm ~/.bash_profile ~/bash_profile_* ~/.bashrc ~/.bash_it ~/.curlrc ~/.gitconfig ~/.vimrc ~/.wgetrc ~/.config/neofetch/config.conf ~/.config/fish/config.fish ~/.config/ranger/rc.conf
 
 echo 'Stowing dem new ones...'
 for stowDir in */; do
